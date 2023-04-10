1a0871de7c7c285b86620df1cca050e0fd2bf33e
unknown <hughesgeorgeking@gmail.com>
2023-04-10 05:03:38 +0000

    changes

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,17 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+import '../components/cart_item.dart';
+import '../models/cart.dart';
+import '../models/shoe.dart';
 
 class CartPage extends StatefulWidget {
   const CartPage({Key? key}) : super(key: key);
 
   @override
+  // ignore: library_private_types_in_public_api
   _CartPageState createState() => _CartPageState();
 }
 
 class _CartPageState extends State<CartPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Text('Cart Page'),
+    return Consumer<Cart>(
+      builder: (context, value, child) => Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Heading
+            Text(
+              'My Cart',
+              style: GoogleFonts.sourceSansPro(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+
+            Expanded(
+              child: ListView.builder(
+                itemCount: value.getUserCart().length,
+                itemBuilder: (context, index) {
+                  // Get individual shoe
+                  Shoe individualShoe = value.getUserCart()[index];
+
+                  // Return the cart item
+                  return CartItem(
+                    shoe: individualShoe,
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
